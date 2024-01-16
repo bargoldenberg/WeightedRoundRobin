e5a5e3b8074f5424d4329591b178b6d37c3b26be
Bar Goldenberg <bargoldenberg@Bars-MacBook-Pro.local>
2024-01-16 20:26:49 +0200

    finished tests

--- wrr.py
+++ wrr.py
@@ -1,4 +1,6 @@
 
+#IMPLEMENTED BY: BAR GOLDENBERG
+
 INF = float('inf')
 
 def find_turn(num_of_players, players_dict, y, rights):
@@ -49,6 +51,11 @@ def weighted_round_robin(rights: list[float], valuations: list[list[float]], y: 
             valuations[i][chosen_item] = None
     return ans
 
+
+
+'''
+TESTS
+'''
 if __name__ == '__main__':
     '''
     Explanation TEST 1 (Different Rights Different Objects)
@@ -81,14 +88,99 @@ if __name__ == '__main__':
     rights=[1,2,4]
     valuations=[[11,11,22,33,44], [11,22,44,55,66], [11,33,22,11,66]]
     y=0.5
+    print("---------------------")
+    print("RUNNING TEST NUMBER 1")
+    print("---------------------")
     arr = weighted_round_robin(rights, valuations, y)
     assert(([[3, 5, 66], [2, 4, 55], [3, 2, 33], [1, 3, 22], [3, 1, 11]].__eq__(arr)))
+    print("---------------------")
+    print("TEST NUMBER 1: SUCCESS")
+    print("---------------------")
     
 
     '''
-    EXPLANATION TEST 2 
+    EXPLANATION TEST 2 (Different Rights Same Items)
+    round 1:
+        player1 = 1/(0+0.5) = 2
+        player2 = 2/(0+0.5) = 4
+        player3 = 4/(0+0.5) = 8
+        player 3 takes item 1 with value 1
+    round 2:
+        player1 = 1/(0+0.5) = 2
+        player2 = 2/(0+0.5) = 4
+        player3 = 4/(1+0.5) = 2.66666
+        player 2 takes item 2 with value 1
+    round 3:
+        player1 = 1/(0+0.5) = 2
+        player2 = 2/(1+0.5) = 1.33333
+        player3 = 4/(1+0.5) = 2.66666
+        player 3 takes item 3 with value 1
     '''
     rights=[1,2,4]
     valuations=[[1,1,1], [1,1,1], [1,1,1]]
     y=0.5
+    print("RUNNING TEST NUMBER 2")
+    print("---------------------")
     arr = weighted_round_robin(rights, valuations, y)
+    assert([[3,1,1],[2,2,1],[3,3,1]].__eq__(arr))
+    print("---------------------")
+    print("TEST NUMBER 2: SUCCESS")
+    print("---------------------")
+
+    '''
+    EXPLANATION TEST 3 (Same Rights Same Items)
+    round 1:
+        player1 = 1/0+0.5 = 2
+        player2 = 1/0+0.5 = 2
+        player3 = 1/0+0.5 = 2
+        player 1 takes item 1 with value 1
+    round 2:
+        player 1 = 1/1+0.5 = 1.33333
+        player 2 = 1/0+0.5 = 2
+        player 3 = 1/0+0.5 = 2
+        player 2 takes item 2 with value 1
+    round 3:
+        player 1 = 1/1+0.5 = 1.33333
+        player 2 = 1/1+0.5 = 1.33333
+        player 3 = 1/0+0.5 = 2
+        player 3 takes item 3 with value 1
+    '''
+    valuations=[[1,1,1], [1,1,1], [1,1,1]]
+    y=0.5
+    rights=[1,1,1]
+    print("RUNNING TEST NUMBER 3")
+    print("---------------------")
+    arr= weighted_round_robin(rights, valuations, y)
+    assert([[1,1,1],[2,2,1],[3,3,1]].__eq__(arr))
+    print("---------------------")
+    print("TEST NUMBER 3: SUCCESS")
+    print("---------------------")
+
+    '''
+    EXPLANATION TEST 4 (Same Rights Different Items)
+    round 1:
+        player1 = 1/(0+0.5) = 2
+        player2 = 2/(0+0.5) = 4
+        player3 = 4/(0+0.5) = 8
+        player 3 takes item 2 with value 3
+    round 2:
+        player1 = 1/(0+0.5) = 2
+        player2 = 2/(0+0.5) = 4
+        player3 = 4/(1+0.5) = 2.66666
+        player 2 takes item 3 with value 3
+    round 3:
+        player1 = 1/(0+0.5) = 2
+        player2 = 2/(1+0.5) = 1.33333
+        player3 = 4/(1+0.5) = 2.66666
+        player 3 takes item 1 with value 1
+    '''
+    valuations=[[1,3,1],[2,40,3],[1,3,2]]
+    rights=[1,2,4]
+    y=0.5
+    print("RUNNING TEST NUMBER 4")
+    print("---------------------")
+    arr= weighted_round_robin(rights, valuations, y)
+    assert([[3,2,3],[2,3,3],[3,1,1]].__eq__(arr))
+    print("---------------------")
+    print("TEST NUMBER 4: SUCCESS")
+    print("---------------------")
